6b847858b72d74ea1367a4717bb3a5f373919e85
CxrlosKenobi <kenobi@ieee.org>
2022-11-19 01:35:48 -0300

    Init proy4ect

---
+++ src/main/java/tarea/app/App.java
@@ -0,0 +1,14 @@
+    package tarea.app;
+
+/**
+ *
+ * @author cxrloskenobi
+ * @author sebasinmas
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
