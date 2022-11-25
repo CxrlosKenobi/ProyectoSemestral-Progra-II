2b2a7c2010b2b4d74c9298e313ec72563ce9047c
SebaSinMas <sebastianbustos90@gmail.com>
2022-11-25 16:51:12 -0300

    Added and loaded a new Font

--- src/main/java/tarea/app/App.java
+++ src/main/java/tarea/app/App.java
@@ -1,7 +1,9 @@
 package tarea.app;
 
 import tarea.app.views.LoadingScreen;
-
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 /**
  *
  * @author cxrloskenobi
@@ -11,8 +13,15 @@ import tarea.app.views.LoadingScreen;
 public class App {
 
     public static void main(String[] args) {
-        
-        //This loads an UI Style for the app
+
+        try {
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("assets/Military Poster.ttf")));
+        } catch (IOException | FontFormatException e) {
+            // Handle exception
+        }
+
+        // This loads an UI Style for the app
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -25,9 +34,6 @@ public class App {
             java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null,
                     ex);
         }
-
-
-
 
         LoadingScreen loadingScreen = new LoadingScreen("Missile Launch System");
     }
