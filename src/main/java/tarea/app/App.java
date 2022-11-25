c6cb67a388a153ec25c11c19dfdb9089c6689893
SebaSinMas <sebastianbustos90@gmail.com>
2022-11-25 16:43:27 -0300

    LoadingScreen can display outside it's own file
    App loads an style for the UI

--- src/main/java/tarea/app/App.java
+++ src/main/java/tarea/app/App.java
@@ -1,5 +1,7 @@
 package tarea.app;
 
+import tarea.app.views.LoadingScreen;
+
 /**
  *
  * @author cxrloskenobi
@@ -9,6 +11,24 @@ package tarea.app;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //This loads an UI Style for the app
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
+        }
+
+
+
+
+        LoadingScreen loadingScreen = new LoadingScreen("Missile Launch System");
     }
 }
