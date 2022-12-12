b856ea3d5da4c2fdeab2e4fae03907b7089f175f
CxrlosKenobi <kenobi@ieee.org>
2022-12-11 21:17:28 -0300

    Initial missile integration

--- src/main/java/tarea/app/packages/elements/Misil.java
+++ src/main/java/tarea/app/packages/elements/Misil.java
@@ -1,5 +1,97 @@
 package tarea.app.packages.elements;
 
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
 public class Misil {
+  private int x;
+  private int y;
+  private double velX;
+  private BufferedImage missileSprite;
+  private boolean positiveDirection;
+
+  public Misil() {
+    this.positiveDirection = true;
+    this.loadMissileImage();
+    this.x = 45;
+    this.y = 0;
+    this.velX = 0;
+  }
+
+  public void paint(Graphics g) {
+    Graphics2D g2D = (Graphics2D) g;
+
+    g2D.drawImage(this.getImage().getScaledInstance(
+        110,
+        30,
+        0),
+      this.x,
+      this.y,
+      null
+    );
+
+    // Add a border to the plane
+    // g2D.setColor(Color.BLACK);
+    // g2D.drawRect(this.x, this.y, 110, 20);
+  }
+
+  public void loadMissileImage() {
+    try {
+      if (positiveDirection) {
+        InputStream is = getClass().getResourceAsStream("../sprites/missileRight.png");
+        missileSprite = ImageIO.read(is);
+
+      } else {
+        InputStream is = getClass().getResourceAsStream("../sprites/missileLeft.png");
+        missileSprite = ImageIO.read(is);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Image getImage() {
+    return this.missileSprite;
+  }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public void update(int controlledY, int controlledVelX) {
+    this.y = controlledY + 45;
+    this.velX = controlledVelX;
+
+    if (positiveDirection) {
+      this.x += this.velX;
+
+      if (this.x > 1061) {
+        positiveDirection = false;
+        this.loadMissileImage();
+      }
     
+    } else {
+      this.x -= this.velX;
+
+      if (this.x < -30) {
+        positiveDirection = true;
+        this.loadMissileImage();
+      }
+    }
+  }
 }
--- src/main/java/tarea/app/views/SimulationPanel.java
+++ src/main/java/tarea/app/views/SimulationPanel.java
@@ -9,15 +9,15 @@ import tarea.app.packages.controllers.*;
 
 public class SimulationPanel extends JPanel implements ActionListener {
     private Avion avion;
+    private Misil misil;
     private Objetivo objetivo;
     private AvionHeightController avionHeightController;
     private AvionThrustController avionThrustController;
-    
     private Timer fpsTimer;
-    // private Image sprite;
 
     public SimulationPanel(AvionHeightController avionHeightController, AvionThrustController avionThrustController) {
         this.avion = new Avion();
+        this.misil = new Misil();
         this.objetivo = new Objetivo();
         this.avionHeightController = avionHeightController;
         this.avionThrustController = avionThrustController;
@@ -35,29 +35,23 @@ public class SimulationPanel extends JPanel implements ActionListener {
 
     @Override
     public void paint(Graphics g) {
-        // g.setBackground(Color.BLACK);
         Graphics2D g2d = (Graphics2D) g;
 
-        // g2d.setBackground(new java.awt.Color(255, 248, 191));
-        // g2d.drawImage(avion.getImage().getScaledInstance(150, 100, 0), 0, 0, null);
         g2d.setColor(new Color(0, 161, 191)); // ground
         g2d.fillRect(0, 500, 1280, 720);
         g2d.setColor(new Color(255, 255, 255)); // sky
         g2d.fillRect(0, 0, 1280, 500);
 
-        
-
         objetivo.paint(g);
+        misil.paint(g);
         avion.paint(g);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         avion.update(avionHeightController.getAltitude(), avionThrustController.getThrust());
+        misil.update(avionHeightController.getAltitude(), avionThrustController.getThrust());
         objetivo.update();
         repaint();
     }
-
-    
-
 }
